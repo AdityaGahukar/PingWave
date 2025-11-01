076811cb6be9e16fa36ceec5ba161f8a45118ac4
AdityaGahukar <adityagahukar@gmail.com>
2025-11-01 10:26:05 +0530

    Implemented initial chat container

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -41,7 +41,7 @@ function ProfileHeader() {
                                 alt="User image"
                                 className="size-full object-cover"    
                             />
-                            <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
+                            <div className="absolute cursor-pointer inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
                                 <span className="text-white text-xs">Change</span>
                             </div>
                         </button>
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -49,5 +49,15 @@ export const useChatStore = create((set, get) => ({
         }
     },
 
-    
+    getMessagesByUserId: async (userId) => {
+        set({isMessagesLoading: true});
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({messages: res.data});
+        } catch (error) {
+            toast.error(error?.response?.data?.message || "Error to get messages");
+        } finally {
+            set({isMessagesLoading: false});
+        }
+    }
 }))
