3487a7454dd9082d0dc6e02ffafecabe74a330f9
AdityaGahukar <adityagahukar@gmail.com>
2025-11-03 23:19:31 +0530

    Implemented online users using Socket.io in frontend and backend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,20 +1,27 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";  // base URL for socket connection (we have same origin in production)
 
 // Zustand store to manage authentication state
 // create a store with initial state and actions to update the state
 // set is used to update the state
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set, get) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
     isLoggingIn: false,
+    onlineUsers: [],
+    socket: null,
 
     checkAuth: async () => {
         try {
             const res = await axiosInstance.get("/auth/check-auth");
             set({ authUser: res.data.user });
+            
+            get().connectSocket();  // connect socket if user is authenticated
         } catch (error) {
             console.log("Error in authCheck:", error);
             set({ authUser: null });
@@ -30,6 +37,8 @@ export const useAuthStore = create((set) => ({
             set({ authUser: res.data });
 
             toast.success("Account created successfully!");
+            
+            get().connectSocket();  // connect socket after signup
         } catch(error) {
             toast.error(error?.response?.data?.message || "Signup failed. Please try again.");
         } finally {
@@ -43,6 +52,8 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("auth/login", data);
             set({ authUser: res.data });
             toast.success("Logged in successfully");
+
+            get().connectSocket();  // connect socket after login
         } catch (error) {
             toast.error(error?.response?.data?.message || "Login failed. Please try again.");
         } finally {
@@ -55,6 +66,8 @@ export const useAuthStore = create((set) => ({
             await axiosInstance.post("auth/logout");
             set({ authUser: null });
             toast.success("Logged out successfully");
+
+            get().disconnectSocket();  // disconnect socket on logout
         } catch (error) {
             toast.error(error?.response?.data?.message || "Logout failed. Please try again.");
         }
@@ -71,4 +84,33 @@ export const useAuthStore = create((set) => ({
             toast.error(message);
         }
     },
+
+    connectSocket: () => {
+        const {authUser} = get();
+
+        if(!authUser || get().socket?.connected) return;  // if no user or socket already connected, do nothing
+
+        // create socket connection if user is authenticated
+        const socket = io(BASE_URL, {
+            withCredentials: true     // this ensures cookies are sent with the connection
+        });  
+
+        socket.connect();  // establish the connection
+
+        set({ socket });  // save socket in the store
+
+        // listen for online users update (event) from server
+        socket.on("getOnlineUsers", (users) => {
+            set({ onlineUsers: users });
+        });
+    },
+
+    disconnectSocket: () => {
+        const { socket } = get();
+        if (socket?.connected) {
+            socket.off("getOnlineUsers");  // remove event listener
+            socket.disconnect();  // disconnect the socket
+            set({ socket: null });  // remove socket from the store
+        }
+    }
 }));
